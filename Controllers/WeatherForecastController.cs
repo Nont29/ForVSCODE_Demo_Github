9101e21830bc9a78c9fb0c42c2c5f67f77a26caa
Nont Sanarmart <nontiwat@ditruz.com>
2026-03-06 15:23:03 +0700

    Add Super Hot

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries =
     [
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Super Hot"
     ];
 
     [HttpGet(Name = "GetWeatherForecast")]
